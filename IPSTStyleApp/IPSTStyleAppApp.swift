3878537c686f0df7cd58d8a47b4fa729d4c19878
Utkarsh Shah <utkarshshah1995@gmail.com>
2025-12-04 21:13:14 -0800

    Initial Commit

---
+++ IPSTStyleApp/IPSTStyleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPSTStyleAppApp.swift
+//  IPSTStyleApp
+//
+//  Created by EXPO on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct IPSTStyleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
